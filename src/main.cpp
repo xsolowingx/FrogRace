48d3a55600084ef0031064619ac4dee00a11526a
Matheus de jesus Leandro de Medeiros <mj.leandro@hotmail.com>
2017-08-22 21:09:02 -0300

    Frog race finished, only missing comments.

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,49 @@
  */
 #include "../include/Frog.h"
 
-int Frog::distanceT_race = 1000;
+int Frog::distanceT_race = 10000;
 
 int main(){
 	srand (time(NULL));
-	char option;
+	std::string option;
 	Frog Frog1,Frog2,Frog3;
 	Frog1.Frogc(1);
 	Frog2.Frogc(2);
 	Frog3.Frogc(3);
-	std::cout<<"Welcome to the frog race game, type 'y' to start the game  or 'n' to exit the game."<<std::endl;
-	std::cin>>option;
+	std::cout<<"Welcome to the frog race game, type 'y' to start the game  or 'n' to exit the game."<<std::endl<<std::endl;
+	while(std::getline(std::cin,option)){
+		if(option == "n"){
+			std::cout<<"quiting..."<<std::endl;
+			return 0;
+		}
+		if(option == "y"){
+			break;
+		}
+		std::cout<<"Invalid option, please type 'y' to start the game or 'n' to exit the game."<<std::endl;
+	}
+
+	while((Frog1.getDistanceT() < Frog::distanceT_race) && 
+		(Frog2.getDistanceT() < Frog::distanceT_race) && 
+		(Frog3.getDistanceT() < Frog::distanceT_race) ){
+		Frog1.jump();
+
+		if(Frog1.getDistanceT() >= Frog::distanceT_race){
+			std::cout<<"The "<<Frog1.getId()<<" was the winner!!!"<<std::endl<<std::endl;
+			break;
+		}
+
+		Frog2.jump();
+		if(Frog2.getDistanceT() >= Frog::distanceT_race){
+			std::cout<<"The "<<Frog2.getId()<<" was the winner!!!"<<std::endl<<std::endl;
+			break;
+		}
+
+		Frog3.jump();
+		if(Frog3.getDistanceT() >= Frog::distanceT_race){
+			std::cout<<"The "<<Frog3.getId()<<" was the winner!!!"<<std::endl<<std::endl;
+			break;
+		}
+	}
 	std::cout<<"The "<<Frog1.getId()<<" went through "<<Frog1.getDistanceT()<<"cm in "<<Frog1.getQuantity_jumps()<<" jumps"<<std::endl;
 	std::cout<<"The "<<Frog2.getId()<<" went through "<<Frog2.getDistanceT()<<"cm in "<<Frog2.getQuantity_jumps()<<" jumps"<<std::endl;
 	std::cout<<"The "<<Frog3.getId()<<" went through "<<Frog3.getDistanceT()<<"cm in "<<Frog3.getQuantity_jumps()<<" jumps"<<std::endl;
